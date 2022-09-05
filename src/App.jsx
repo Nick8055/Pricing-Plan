73a7e144a7a3b194b1b2c5d8f0cae0e76b248c93
Nicholas <nicholasrebeiro@gmail.com>
2022-09-05 23:22:59 +0530

    C6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 
-const Plan = ({stylingType, no_of_admins, no_of_doctors, plan_price, feature_type, feature1, feature2, feature3}) => {
+const Plan = ({no_of_admins, no_of_doctors, plan_price, feature_type, feature1, feature2, feature3}) => {
 
   //For 'stylingType' parameter, use integer value 0 for highlighted styling or integer value 1 for normal styling
 
+  const [stylingType, setIsHover] = useState(true);
+
+  const handleMouseEnter = () => {
+     setIsHover(false);
+  };
+  const handleMouseLeave = () => {
+     setIsHover(true);
+  };
+
   return(
-    <div className={stylingType ? 'PlanContainer' : 'PlanContainerAlternate'}>
+    <div className={stylingType ? 'PlanContainer' : 'PlanContainerAlternate'} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className= {stylingType ? 'PlanContainerPart1' : 'PlanContainerPart1Alternate'}>
         <h1 className={stylingType ? 'AdminsDoctors' : 'AdminsDoctorsAlternate'}>{no_of_admins} Admin(s) . {no_of_doctors} Doctor(s)</h1>
         <h1 className={stylingType ? 'PlanPrice' : 'PlanPriceAlternate'}>₹{plan_price} / Annum</h1>
@@ -22,13 +32,13 @@ const Plan = ({stylingType, no_of_admins, no_of_doctors, plan_price, feature_typ
 }
 
 const App = () => {
+
   return (
     <div className="App">
       <h1 className='Title'>Pricing</h1>
-      <h1 className='Subtitle'>Additional user - 1000 /Annum/User Additional clinic- 5000 /Annum/Additional Clinic </h1>
-      <div className='PlanContainerFlexbox'>
+      <div  className='PlanContainerFlexbox'>
+
         <Plan 
-        stylingType={1}
         no_of_admins={1} 
         no_of_doctors={2} 
         plan_price={6000} 
@@ -37,18 +47,28 @@ const App = () => {
         feature2={"SMS Service"}
         feature3={"24/7 Customer Support"}
         />
+
         <Plan 
-        stylingType={0}
-        no_of_admins={2} 
-        no_of_doctors={2} 
-        plan_price={6000}
-        feature_type={"Basic"} 
-        feature1={"Client Management Toolkit"}
-        feature2={"SMS Service"}
-        feature3={"24/7 Customer Support"}
+        no_of_admins={1} 
+        no_of_doctors={4} 
+        plan_price={8000}
+        feature_type={"Premium"} 
+        feature1={"Whatsapp Service"}
+        feature2={"Expense Tracker"}
+        // feature3={"24/7 Customer Support"}
         />
+
         <Plan 
-        stylingType={1}
+        // no_of_admins={3} 
+        // no_of_doctors={2} 
+        // plan_price={6000}
+        feature_type={"Enterprise"} 
+        // feature1={"Client Management Toolkit"}
+        // feature2={"SMS Service"}
+        // feature3={"24/7 Customer Support"}
+        />
+
+        {/* <Plan 
         no_of_admins={3} 
         no_of_doctors={2} 
         plan_price={6000}
@@ -56,9 +76,9 @@ const App = () => {
         feature1={"Client Management Toolkit"}
         feature2={"SMS Service"}
         feature3={"24/7 Customer Support"}
-        />
-                <Plan 
-        stylingType={1}
+        /> */}
+
+        {/* <Plan 
         no_of_admins={3} 
         no_of_doctors={2} 
         plan_price={6000}
@@ -66,18 +86,10 @@ const App = () => {
         feature1={"Client Management Toolkit"}
         feature2={"SMS Service"}
         feature3={"24/7 Customer Support"}
-        />
-                <Plan 
-        stylingType={0}
-        no_of_admins={3} 
-        no_of_doctors={2} 
-        plan_price={6000}
-        feature_type={"Basic"} 
-        feature1={"Client Management Toolkit"}
-        feature2={"SMS Service"}
-        feature3={"24/7 Customer Support"}
-        />
+        /> */}
+
       </div>
+      <h1 className='Footer'>Additional user - 1000 /Annum/User <br></br> Additional clinic- 5000 /Annum/Additional Clinic </h1>
     </div>
   );
 }
